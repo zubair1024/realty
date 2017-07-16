3c8f033ebac9426192c7e4bb9fdbacaf17c6ca16
Zubair Ahmed <zubair1024@gmail.com>
2017-07-17 01:30:52 +0400

    FCD

--- src/app-footer/app-footer.js
+++ src/app-footer/app-footer.js
@@ -1,3 +1,3 @@
 export class AppFooter {
-    constructor() { }
+  constructor() { }
 }
--- src/app.js
+++ src/app.js
@@ -1,5 +1,34 @@
+import { Aurelia, inject } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+
+@inject(Aurelia, Router)
 export class App {
-  constructor() {
-    this.message = 'Hello World!';
+  constructor(aurelia, router) {
+    this.aurelia = aurelia;
+    this.router = router;
+  }
+
+  configureRouter(config, router) {
+    config.map([
+      {
+        route: ['', 'home'],
+        name: 'home',
+        moduleId: 'home/home',
+        title: 'Home'
+      },
+      {
+        route: 'submit',
+        name: 'submit',
+        moduleId: 'submit/submit',
+        title: 'Submit Property'
+      },
+      {
+        route: 'contact',
+        name: 'contact',
+        moduleId: 'contact/contact',
+        title: 'Contact Us'
+      }
+    ]);
+    this.router = router;
   }
 }
