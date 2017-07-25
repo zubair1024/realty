4b1f2d41f577c70a3823d7c4313409f068fbdbff
Zubair Ahmed <zubair1024@gmail.com>
2017-07-25 10:12:00 +0400

    adding emailing services

--- index.js
+++ index.js
@@ -20,6 +20,22 @@ app.use(compression());
 
 
 /**
+ * Email Transporter
+ */
+const nodemailer = require('nodemailer');
+
+// create reusable transporter object using the default SMTP transport
+global.transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // secure:true for port 465, secure:false for port 587
+  auth: {
+    user: 'thepropertybuyingcompanyae@gmail.com',
+    pass: 'onlyvimal1'
+  }
+});
+
+/**
  * CORS setup 
  */
 
