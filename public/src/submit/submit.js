719542aa7517c26e3842ef11235f09b44c293cb2
Zubair Ahmed <zubair1024@gmail.com>
2017-08-11 01:31:50 +0530

    adding tnc check and changing the logo

--- public/src/submit/submit.js
+++ public/src/submit/submit.js
@@ -11,6 +11,8 @@ export class Submit {
     }
   }
 
+  tncCheck = false;
+
   //placeholders
   selectedFiles;
   titleDeed;
@@ -28,52 +30,52 @@ export class Submit {
       //location information
     case 1:
       if (!this.model.location.address || this.model.location.address === '') {
-        valid = false;
-        alert('Please fill in the ADDRESS field');
-      } else {
-        if (!this.model.location.unit || this.model.location.unit === '') {
           valid = false;
-          alert('Please fill in the UNIT field');
+          alert('Please fill in the ADDRESS field');
         } else {
-          !callback && $('.submit-property__steps a[href="#submit-property-2"]').tab('show');
-        }
-      }
-      break;
-      //contact information
-    case 2:
-      if (!this.model.contact.name || this.model.contact.name === '') {
-        valid = false;
-        alert('Please fill in the NAME field');
-      } else {
-        if (!this.model.contact.email || this.model.contact.email === '') {
-          valid = false;
-          alert('Please fill in the EMAIL field');
-        } else {
-          if (!this.model.contact.contactNo || this.model.contact.contactNo === '') {
+          if (!this.model.location.unit || this.model.location.unit === '') {
             valid = false;
-            alert('Please fill in the CONTACT NUMBER field');
+            alert('Please fill in the UNIT field');
           } else {
             !callback && $('.submit-property__steps a[href="#submit-property-2"]').tab('show');
           }
         }
-      }
+      break;
+      //contact information
+    case 2:
+      if (!this.model.contact.name || this.model.contact.name === '') {
+          valid = false;
+          alert('Please fill in the NAME field');
+        } else {
+          if (!this.model.contact.email || this.model.contact.email === '') {
+            valid = false;
+            alert('Please fill in the EMAIL field');
+          } else {
+            if (!this.model.contact.contactNo || this.model.contact.contactNo === '') {
+              valid = false;
+              alert('Please fill in the CONTACT NUMBER field');
+            } else {
+              !callback && $('.submit-property__steps a[href="#submit-property-2"]').tab('show');
+            }
+          }
+        }
       break;
       //property information
     case 3:
       if (!this.model.information.title || this.model.information.title === '') {
-        valid = false;
-        alert('Please fill in the TITLE field');
-      } else {
-        if (!this.model.information.view || this.model.information.view === '') {
           valid = false;
-          alert('Please fill in the VIEW field');
+          alert('Please fill in the TITLE field');
         } else {
-          if (!this.model.information.squareFeet || this.model.information.squareFeet === '') {
+          if (!this.model.information.view || this.model.information.view === '') {
             valid = false;
-            alert('Please fill in the SQUARE FEET field');
+            alert('Please fill in the VIEW field');
+          } else {
+            if (!this.model.information.squareFeet || this.model.information.squareFeet === '') {
+              valid = false;
+              alert('Please fill in the SQUARE FEET field');
+            }
           }
         }
-      }
       break;
     default:
         //do nothing
@@ -100,36 +102,38 @@ export class Submit {
 
   finalValidationAnSubmit(images) {
     let me = this;
-    me.validateStep(1, (f1) => {
-      console.log('step 1 is valid');
-      f1 && me.validateStep(2, (f2) => {
-        console.log('step 2 is valid');
-        f2 && me.validateStep(3, (f3) => {
-          console.log('step 3 is valid');
-          //neeed to do something aboutt the images before doing this
-          let formData = new FormData();
-          formData.append('images', me.deed[0], `deed_${me.model.information.title}.jpg`);
-          for (let i = 0; i < me.selectedFiles.length; i++) {
-            formData.append('images', me.selectedFiles[i], `property${i}_${me.model.information.title}.jpg`);
-          }
-          formData.append('model', JSON.stringify(me.model));
-          $.ajax({
-            url: '/property',
-            method: 'POST',
-            data: formData,
-            contentType: false,
-            processData: false,
-            success: function(data) {
-              console.log('data');
-              f3 && $('#submitted').tab('show');
-            },
-            error: function(err) {
-              console.log(err);
+    if (this.tncCheck) {
+      me.validateStep(1, (f1) => {
+        console.log('step 1 is valid');
+        f1 && me.validateStep(2, (f2) => {
+          console.log('step 2 is valid');
+          f2 && me.validateStep(3, (f3) => {
+            console.log('step 3 is valid');
+            //neeed to do something aboutt the images before doing this
+            let formData = new FormData();
+            formData.append('images', me.deed[0], `deed_${me.model.information.title}.jpg`);
+            for (let i = 0; i < me.selectedFiles.length; i++) {
+              formData.append('images', me.selectedFiles[i], `property${i}_${me.model.information.title}.jpg`);
             }
+            formData.append('model', JSON.stringify(me.model));
+            $.ajax({
+              url: '/property',
+              method: 'POST',
+              data: formData,
+              contentType: false,
+              processData: false,
+              success: function(data) {
+                console.log('data');
+                f3 && $('#submitted').tab('show');
+              },
+              error: function(err) {
+                console.log(err);
+              }
+            });
           });
         });
       });
-    });
+    }
   }
 }
 
