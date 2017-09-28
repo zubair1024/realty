dafdbd6c8d789f5e05a54d4c225b1805647f461f
Zubair Ahmed <zubair1024@gmail.com>
2017-09-28 17:55:34 +0400

    adjusting contact number formation

--- public/scripts/app-bundle.js
+++ public/scripts/app-bundle.js
@@ -473,7 +473,7 @@ define('submit/submit',['exports'], function (exports) {
         valid = false;
         this.validation.contact.contactNo = true;
       } else {
-        var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+        var phoneno = /\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$/;
         if (!this.model.contact.contactNo.match(phoneno)) {
           valid = false;
           this.validation.contact.contactNo = true;
