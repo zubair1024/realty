fe6e95b62302ec3864866e6127ffa07fc1aa838d
Zubair Ahmed <zubair1024@gmail.com>
2017-10-11 23:29:39 +0400

    adding a slight change

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ app.use(compression());
 app.use(function (req, res, next) {
   agent = req.headers['user-agent'];
   if (agent.indexOf('Safari') > -1 && agent.indexOf('Chrome') == -1 && agent.indexOf('OPR') == -1) {
+    console.log('Apple Fix');
     res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
     res.header('Pragma', 'no-cache')
     res.header('Expires', 0)
